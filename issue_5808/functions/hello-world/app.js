550ce78814299645079937fd4d624e7f35074eed
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-04-06 22:07:30 +0100

    issue_5808

---
+++ issue_5808/functions/hello-world/app.js
@@ -0,0 +1,16 @@
+let response;
+exports.handler = async (event, context) => {
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
